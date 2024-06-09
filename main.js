1a68d41448342341466c9122a3826355f277ecf1
Leyli_Alakbarova <leyaleka3@gmail.com>
2024-06-09 21:45:25 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// console.log(7);
+// document.getElementById().addEventListener('click' , ()=>{
+//     console.log('clicK eeldim');
+//     document.getElementById().innerText= 'i was clicked';
+
+// })
